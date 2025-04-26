58779af6d1c361452a87b3d57a000ba76b13e04d
Andy Trainor <me@andrewtrainor.kiwi>
2025-04-27 11:55:19 +1200

    add publishing script (#14)

--- src/scripts/ci/publish.sh
+++ src/scripts/ci/publish.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 # This script is used to publish the package to npm.
 echo running publish.sh
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$branch" = "latest" ]; then
+  echo "Publishing to npm..."
+  npm publish --tag beta #fix up when ready
+elif [ "$branch" = "beta" ]; then
+  echo "Publishing beta to npm..."
+  npm publish --tag beta
+else
+  echo "Not on latest or beta branch, skipping publish."
+fi
